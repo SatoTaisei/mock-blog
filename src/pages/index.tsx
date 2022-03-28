d8fb69d3b80c7f057f11a7788ad54795c7a87700
SatoTaisei <t.sei43rm@gmail.com>
2022-03-28 22:09:34 +0900

    ✨ microCMSとの連携

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,29 @@
-import type { NextPage } from "next";
+import type { NextPage, GetStaticProps } from "next";
+import type { Article } from "../types/article";
+import { client } from "../libs/client";
 
-const Home: NextPage = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+type Props = {
+  articles: Article;
 };
 
+const Home: NextPage<Props> = ({ articles }) => {
+  return (
+    <>
+      <h1 className="text-3xl font-bold">タイトル：{articles.title}</h1>
+      <p className="text-l text-gray-500">{articles.body}</p>
+    </>
+  );
+};
 export default Home;
+
+export const getStaticProps: GetStaticProps = async () => {
+  const data = await client
+    .get({
+      endpoint: "articles",
+      contentId: "63ekd465z",
+    })
+    .then((res) => res)
+    .catch(() => null);
+
+  return { props: { articles: data } };
+};
