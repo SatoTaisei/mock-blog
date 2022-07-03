7b8c421ef099cfb483b89bc2e4910b2f14259881
SatoTaisei <t.sei43rm@gmail.com>
2022-07-04 01:15:29 +0900

    記事一覧画面の追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,19 +2,40 @@ import type { NextPage, GetStaticProps } from "next";
 import type { Article } from "@/types/article";
 import { client } from "@/libs/client";
 import { Header } from "@/components/Header";
+import { Footer } from "@/components/Footer";
 
 type Props = {
   articles: Article;
 };
 
 const Home: NextPage<Props> = ({ articles }) => {
+  const substring200TextArray: string[] = [];
+  articles.map(
+    (article, index) =>
+      (substring200TextArray[index] = article.body.substring(0, 199))
+  );
+
   return (
     <>
       <Header />
-      <main className="flex flex-col justify-center w-10/12 my-8 mx-auto">
-        <h1 className="text-3xl font-bold">Title：{articles.title}</h1>
-        <p className="text-l text-gray-500">{articles.body}</p>
+      <main className="w-10/12 min-h-full my-8 mx-auto flex flex-wrap">
+        {articles.map((article, index) => {
+          return (
+            <div
+              key={index}
+              className="w-80 h-60 border-2 rounded-2xl py-2 mx-8 my-4"
+            >
+              <h2 className="leading-relaxed text-3xl font-bold px-4">
+                Title：{article.title}
+              </h2>
+              <p className="text-l text-gray-500 py-2 px-4">
+                {substring200TextArray[index]}
+              </p>
+            </div>
+          );
+        })}
       </main>
+      <Footer />
     </>
   );
 };
@@ -24,10 +45,9 @@ export const getStaticProps: GetStaticProps = async () => {
   const data = await client
     .get({
       endpoint: "articles",
-      contentId: "63ekd465z",
     })
     .then((res) => res)
     .catch(() => null);
 
-  return { props: { articles: data } };
+  return { props: { articles: data.contents } };
 };
