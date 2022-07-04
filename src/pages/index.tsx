098c82adac5d2402e52c615b481e4463be1b410b
SatoTaisei <t.sei43rm@gmail.com>
2022-07-04 22:35:51 +0900

    記事一覧の修正：Linkを追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,10 @@ import type { Article } from "@/types/article";
 import { client } from "@/libs/client";
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
+import Link from "next/link";
 
 type Props = {
-  articles: Article;
+  articles: Article[];
 };
 
 const Home: NextPage<Props> = ({ articles }) => {
@@ -18,22 +19,28 @@ const Home: NextPage<Props> = ({ articles }) => {
   return (
     <>
       <Header />
-      <main className="w-10/12 min-h-full my-8 mx-auto flex flex-wrap">
-        {articles.map((article, index) => {
-          return (
-            <div
-              key={index}
-              className="w-80 h-60 border-2 rounded-2xl py-2 mx-8 my-4"
-            >
-              <h2 className="leading-relaxed text-3xl font-bold px-4">
-                Title：{article.title}
-              </h2>
-              <p className="text-l text-gray-500 py-2 px-4">
-                {substring200TextArray[index]}
-              </p>
-            </div>
-          );
-        })}
+      <main className="w-10/12 min-h-full my-8 mx-auto">
+        <ul className="flex flex-wrap justify-center">
+          {articles.map((article, index) => {
+            return (
+              <li
+                key={index}
+                className="w-80 h-60 border-2 rounded-2xl pt-2 pb-4 mx-8 my-4 hover:border-blue-500 hover:border-4"
+              >
+                <Link href={`/articles/${article.id}`}>
+                  <a>
+                    <h2 className="leading-relaxed text-3xl font-bold px-4">
+                      Title：{article.title}
+                    </h2>
+                    <p className="text-l text-gray-500 py-2 px-4">
+                      {substring200TextArray[index]}
+                    </p>
+                  </a>
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
       </main>
       <Footer />
     </>
